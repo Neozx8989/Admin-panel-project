7caec9e6316b72752d4df85c3b6b998eb197d25c
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-02-01 12:49:31 +0800

    page folder uusged component oo orulj uguw

--- admin-panel-project/frontend/src/components/NewProduct.jsx
+++ admin-panel-project/frontend/src/components/NewProduct.jsx
@@ -2,6 +2,7 @@ export default function NewProduct () {
     return (
         <div>
             <h1> New Product Page </h1>
+            {/* Add product buyu hereglegch shineer New product heseg hiih heseg */}
         </div>
     )
 }
--- admin-panel-project/frontend/src/components/NewUser.jsx
+++ admin-panel-project/frontend/src/components/NewUser.jsx
@@ -2,6 +2,7 @@ export default function NewUser() {
     return (
         <div>
             <h1>New User Page</h1>
+            {/* Add users buyu shineer hereglegch burtgeh heseg hiih component */}
         </div>
     )
 }
